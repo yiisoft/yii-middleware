5e9687edeab1a072c530315f11ff41667fe882a8
Alexander Makarov <sam@rmcreative.ru>
2023-05-20 19:25:58 +0400

    Add docs for LocaleEvent

--- src/Event/LocaleEvent.php
+++ src/Event/LocaleEvent.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Middleware\Event;
 
+use Yiisoft\Yii\Middleware\Locale;
+
+/**
+ * Raised when {@see Locale} middleware determines the locale to use.
+ * Use this event to configure locale of extra services.
+ */
 final class LocaleEvent
 {
+    /**
+     * @param string $locale Locale determined by {@see Locale} middleware.
+     */
     public function __construct(private string $locale)
     {
     }
 
+    /**
+     * Get locale determined by {@see Locale} middleware.
+     *
+     * @return string Locale to use.
+     */
     public function getLocale(): string
     {
         return $this->locale;
